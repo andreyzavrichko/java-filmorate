b871508cd1c8c3b602b363a4e023aeaf90e69487
Andrey <andrey@zavrichko.dev>
2025-09-24 20:08:20 +0300

    фикс замечаний

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/GenreService.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.storage.dao.GenreStorage;
 
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class GenreService {
